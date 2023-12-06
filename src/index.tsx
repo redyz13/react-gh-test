5aa75a5749a41e1cd9693fded2f6bb1b6455e428
Antonio <35410603+redyz13@users.noreply.github.com>
2023-12-06 23:02:24 +0100

    modified:   tsconfig.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './main/index.css';
+import App from './main/App';
+import reportWebVitals from './main/reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
