e2953b133bfb11f6682bd20b0f1d6b442ec6d8ca
Antonio <35410603+redyz13@users.noreply.github.com>
2023-12-18 12:47:39 +0100

    modified:   ../client/App.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -14,7 +14,7 @@ function App(): JSX.Element {
       .then((response) => response.json())
       .then((data) => setPersons(data))
       .catch((error) => console.error('Error fetching data:', error));
-  }, []);
+  }, [remoteServerUrl]);
 
   return (
     <div className="App">
